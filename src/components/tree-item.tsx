ac48eb500bb4b8544c1c32ae74045ff179613581
yangjie <jie.yang@witontek.com>
2023-03-29 17:21:19 +0800

    🚧:

--- src/components/tree-item.tsx
+++ src/components/tree-item.tsx
@@ -8,6 +8,7 @@ export const TreeItem: React.FC<NodeItem> = (props) => {
         <div className={styles.tree_line}>
             <span className={styles.leaf_name}>{name}</span>
         </div>
+        { (children as NodeItem[]).length > 0 && <TreeItem /> }
     </>
     // const hasChild = (children as NodeItem[])?.length > 0 ?
     //     ;
