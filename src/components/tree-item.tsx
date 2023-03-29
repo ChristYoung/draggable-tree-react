8bb874ee5af4a6be9364c5ec00da69176edaa890
yangjie <jie.yang@witontek.com>
2023-03-29 13:00:06 +0800

    ✨ feat: update

--- src/components/tree-item.tsx
+++ src/components/tree-item.tsx
@@ -1,6 +1,15 @@
 import { NodeItem } from '@/types';
+import styles from '@/styles/Home.module.css'
 
 export const TreeItem: React.FC<NodeItem> = (props) => {
     const { name, ...res } = props;
-    return <><h3>{name}</h3></>
+    return (
+        <>
+        <div className={styles.tree_line}>
+            <span className="arrow"></span>
+            <span>{name}</span>
+            <span className="icon human_icon" ></span>
+        </div>
+        </>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { TreeList } from '@/components/tree';
 import { MockData } from '@/mock';
 import { NodeItem } from '@/types';
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   const eles = MockData as NodeItem[];
-
   return (
     <div>
       <h1 className=''>Home</h1>
