045ddc352b67067da6ddfe152e5ff3a6cfddb9bb
yangjie <jie.yang@witontek.com>
2023-03-31 15:41:26 +0800

    🎉 init: marquee

--- src/components/marquee/index.tsx
+++ src/components/marquee/index.tsx
@@ -1,9 +1,14 @@
 import { HTMLAttributes } from 'react';
+import styles from './style.module.css'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 export const Marquee: React.FC<Props> = (props: Props) => {
+    const { children } = props;
     return (
-        <div> </div>
+       <div className={styles.marquee}>
+         <div className={styles.content}>{children}</div>
+         <div className={styles.content}>{children}</div>
+       </div>
     )
 }
---
+++ src/pages/marqueePage.tsx
@@ -0,0 +1,9 @@
+import { Marquee } from '@/components/marquee';
+
+export default function MarqueePage() {
+    return (
+        <>
+            <Marquee>千万人康师傅就不会时代峰峻</Marquee>
+        </>
+    )
+}
