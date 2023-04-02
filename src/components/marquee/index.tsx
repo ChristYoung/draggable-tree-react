ff4b0d8ceadb847064efbf08e709b5ab2a705aa5
yangjie <jie.yang@witontek.com>
2023-04-02 15:31:59 +0800

    ✨ feat:

--- src/components/marquee/index.tsx
+++ src/components/marquee/index.tsx
@@ -6,23 +6,28 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   animationDelay?: CSSProperties['animationDelay'];
   direction?: 'left' | 'right';
   grandientColor?: string;
-  grandientWidth?: CSSProperties['width']
+  grandientWidth?: CSSProperties['width'];
+  pauseOnHover?: boolean;
 }
 
 export const Marquee: React.FC<Props> = (props: Props) => {
     const {
       direction = 'left',
-      grandientColor, grandientWidth,
+      grandientColor, grandientWidth, pauseOnHover,
       animationDelay, children, className, ...restProps } = props;
     const contentStyles: CSSProperties = {
       animationDelay,
       animationDirection: direction === 'right' ? 'reverse' : undefined
     };
     return (
-       <div className={classNames(styles.marquee, className)} {...restProps}>
+       <div className={classNames(styles.marquee, className, { [styles.pauseOnHover]: pauseOnHover})} {...restProps}>
          <div className={styles.content} style={contentStyles}>{children}</div>
          <div className={styles.content} style={contentStyles}>{children}</div>
-         <div className={classNames(
+         
+         {
+          grandientColor && (
+            <>
+        <div className={classNames(
           styles.overlay,
           styles.leftOverLay,
          )} style={{ background: `linear-gradient(90deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%);`, width: grandientWidth }}
@@ -31,6 +36,9 @@ export const Marquee: React.FC<Props> = (props: Props) => {
           styles.overlay,
           styles.rightOverLay,
          )} style={{ background: `linear-gradient(270deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%);`, width: grandientWidth }}></div>
+            </>
+          )
+         }
        </div>
     )
 }
--- src/pages/marqueePage.tsx
+++ src/pages/marqueePage.tsx
@@ -4,7 +4,12 @@ import styles from '../styles/marqueePage.module.css'
 export default function MarqueePage() {
     return (
         <>
-            <Marquee className={styles.marquee} animationDelay='1s' direction='right' grandientColor='red' grandientWidth='100px'>
+            <Marquee className={styles.marquee}
+                pauseOnHover
+                animationDelay='1s'
+                direction='right'
+                grandientColor='red'
+                grandientWidth='100px'>
                 <div className={styles.list}>
                     {Array(10).fill(1).map((item, index) => 
                         <div key={index} className={styles.item}>{item}</div>
