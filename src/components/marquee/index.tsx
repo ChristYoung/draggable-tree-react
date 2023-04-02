0c666d3228d25408412427ac0c3777efa01ccb5b
yangjie <jie.yang@witontek.com>
2023-04-02 15:49:38 +0800

    ✨ feat: duration

--- src/components/marquee/index.tsx
+++ src/components/marquee/index.tsx
@@ -1,8 +1,9 @@
-import { CSSProperties, HTMLAttributes } from 'react';
+import { CSSProperties, HTMLAttributes, useEffect, useRef, useState } from 'react';
 import styles from './style.module.css'
 import classNames from 'classnames';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
+  speed? : number;
   animationDelay?: CSSProperties['animationDelay'];
   direction?: 'left' | 'right';
   grandientColor?: string;
@@ -12,13 +13,28 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 export const Marquee: React.FC<Props> = (props: Props) => {
     const {
+      speed = 20,
       direction = 'left',
       grandientColor, grandientWidth, pauseOnHover,
       animationDelay, children, className, ...restProps } = props;
+
+    const [contentWidth, setContentWidth] = useState<number>(0);
+
+    const contentRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      if (contentRef.current) {
+        setContentWidth(contentRef.current.getBoundingClientRect().width)
+      }
+    })
+
+    const duration  = contentWidth / speed;
+
     const contentStyles: CSSProperties = {
       animationDelay,
-      animationDirection: direction === 'right' ? 'reverse' : undefined
+      animationDirection: direction === 'right' ? 'reverse' : undefined,
+      animationDuration: `${duration}s`
     };
+
     return (
        <div className={classNames(styles.marquee, className, { [styles.pauseOnHover]: pauseOnHover})} {...restProps}>
          <div className={styles.content} style={contentStyles}>{children}</div>
@@ -30,12 +46,12 @@ export const Marquee: React.FC<Props> = (props: Props) => {
         <div className={classNames(
           styles.overlay,
           styles.leftOverLay,
-         )} style={{ background: `linear-gradient(90deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%);`, width: grandientWidth }}
+         )} style={{ background: `linear-gradient(90deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%)`, width: grandientWidth }}
          ></div>
          <div className={classNames(
           styles.overlay,
           styles.rightOverLay,
-         )} style={{ background: `linear-gradient(270deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%);`, width: grandientWidth }}></div>
+         )} style={{ background: `linear-gradient(270deg, ${grandientColor} 0%, rgba(255, 255, 255, 0) 100%)`, width: grandientWidth }}></div>
             </>
           )
          }
--- src/pages/marqueePage.tsx
+++ src/pages/marqueePage.tsx
@@ -8,7 +8,7 @@ export default function MarqueePage() {
                 pauseOnHover
                 animationDelay='1s'
                 direction='right'
-                grandientColor='red'
+                grandientColor='#F8FBFD'
                 grandientWidth='100px'>
                 <div className={styles.list}>
                     {Array(10).fill(1).map((item, index) => 
