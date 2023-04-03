694560bcf3bf94cc6883aa5b6246478af08dcfd6
yangjie <jie.yang@witontek.com>
2023-04-03 16:48:21 +0800

    🚧: dnd drag

--- src/components/tree-item.tsx
+++ src/components/tree-item.tsx
@@ -2,14 +2,18 @@ import { NodeItem } from '@/types';
 import styles from  '../styles/TreeItem.module.css'
 import { useState } from 'react';
 import { RightOutlined, DownOutlined, FolderFilled, HolderOutlined } from '@ant-design/icons';
+import { useDraggable } from '@dnd-kit/core';
 
 
 export const TreeItem: React.FC<NodeItem> = (props) => {
-    const { name, children, level, expanded, type } = props;
+    const { name, children, level, expanded, type, id } = props;
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
     const [expand, setExpand] = useState(expanded);
     return (
         <>
-            <div className={styles.tree_line} style={{ paddingLeft: `${level * 26}px` }} onClick={() => setExpand(!expand)}>
+            <div className={styles.tree_line}
+             {...listeners} {...attributes} ref={setNodeRef}
+             style={{ paddingLeft: `${level * 26}px` }} onClick={() => setExpand(!expand)}>
                 {
                     // 箭头展示
                     (type === 'DIR' && children.length > 0) && <span className={styles.arrow}>{ expand ? <DownOutlined /> : <RightOutlined /> }</span>
